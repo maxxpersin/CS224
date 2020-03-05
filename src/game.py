a5bbc497a2a96dd493f5381484aec91e48dd20ec
maholat <48300427+maholat@users.noreply.github.com>
2020-03-04 21:07:06 -0600

    Update game.py

--- src/game.py
+++ src/game.py
@@ -1,6 +1,15 @@
+# Main 
 # 
 # Battle-Py!
 #
 # Authors: Max Ahola, Douglas Krouth, Alyssa Oswald, Maxx Persin
 #
 #
+
+Ship_num = 5
+
+grid= []
+for i in range(10):
+    grid.append(['0','0','0','0','0','0','0','0','0','0'])
+grid[3][4]=1
+print grid
