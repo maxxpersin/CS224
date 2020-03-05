3ff3f3d085940da9e7a140590a6ccd4e387af4d0
Maxx Persin <36431722+maxxpersin@users.noreply.github.com>
2020-03-05 13:40:19 -0600

    first grid of buttons working, need to add disable function

--- src/game.py
+++ src/game.py
@@ -7,18 +7,30 @@
 #
 
 from tkinter import *
+from player import Player
 
-def create_board():
-    arr = []
+
+def create_board(root):
+    arr = [0] * 12
+    for i in range(0, 12):
+        arr[i] = [0] * 12
+        for j in range(0, 12):
+            b = Button(root, text='{}, {}'.format(i, j))
+            b.grid(row=j, column=i)
+            arr[i][j] = b
+
+    return arr
+
 
 root = Tk()
 root.grid()
 
-board1 = create_board()
+board1 = create_board(root)
+board2 = create_board(root)
 
 root.mainloop()
 
-Ship_num = 5
+# Ship_num = 5
 
 # grid= []
 # for i in range(10):
