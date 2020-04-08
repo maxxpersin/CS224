573ac219d16e5d97a006bfad25d0a66962f9cbf4
alysoswa <alyssao.alyssao@gmail.com>
2020-04-08 00:03:57 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -6,7 +6,7 @@
 #
 #
 
-from tkinter import *
+
 from player import Player
 from bot import Bot
 
@@ -15,7 +15,7 @@ def play_game(player, bot):
     player.create_board()
     bot.print_board()
 
-    player.make_guess(int(input('Enter X Coordinate: ')), int(input('Enter Y Coordinate')), bot)
+    player.make_guess(int(input('Enter X Coordinate: ')), int(input('Enter Y Coordinate: ')), bot)
     player.print_board()
     bot.print_board()
 
@@ -28,3 +28,8 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+
+#bug: using the game play V x = 2,5 y start = 1 y end = 4 only changes the first two spots not all 4
+# update: the whole middle of the board doesn't change
