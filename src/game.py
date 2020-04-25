9120de02a3e1903decdaf0c25f52cc9f6df48983
alysoswa <alyssao.alyssao@gmail.com>
2020-04-24 23:09:59 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -15,10 +15,24 @@ def play_game(player, bot):
     player.create_board()
     bot.print_board()
 
-    player.make_guess(int(input('Enter X Coordinate: ')), int(input('Enter Y Coordinate: ')), bot)
-    player.print_board()
-    bot.print_board()
+    while player_win(player) == False: # and bot_win(bot) == False:
+        player.make_guess(int(input('Enter X Coordinate: ')), int(input('Enter Y Coordinate: ')), bot)
+        player.print_board()
+        bot.print_board()
+    
+    
+def player_win(player):
+    for i in player.ships:
+        if i.sunk == False:
+            return False
+    return True
 
+# uncomment when bot class is done and has a ships attribute
+#def bot_win(bot):
+#    for i in bot.ships:
+#        if i.sunk == False:
+#            return False
+#    return True
 
 def main():
     player = Player(input('Enter your name: '))
@@ -29,7 +43,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-#bug: using the game play V x = 2,5 y start = 1 y end = 4 only changes the first two spots not all 4
-# update: the whole middle of the board doesn't change
